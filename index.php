2743923919314c39a335158d258e93225f6a2b10
xTSubZero <lima.rafael67@hotmail.com>
2019-06-02 14:16:38 -0300

    Adicionando locutores da radio no index

--- index.php
+++ index.php
@@ -62,10 +62,10 @@
 
 				<div class="carousel-item active">
 				<svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"/></svg>	
-				<img class="d-block w-100" src="images/paramore" alt="First slide">
+				<img class="d-block w-100" src="images/Raça" alt="First slide">
 					<div class="container">
-						<div class="carousel-caption text-left">
-							<h1>Paramore</h1>
+						<div class="carousel-caption ">
+							<h1>Raça negra</h1>
 							<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
 							<p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
 						</div>
@@ -74,10 +74,10 @@
 
 				<div class="carousel-item carousel2">
 					<svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"/></svg>
-					<img class="d-block w-100" src="images/slipknot" alt="First slide">
+					<img class="d-block w-100" src="images/gustavo" alt="First slide">
 					<div class="container">
-						<div class="carousel-caption">
-							<h1>Slipknot</h1>
+						<div class="carousel-caption text-left">
+							<h1>Gusttavo Lima</h1>
 							<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
 							<p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
 						</div>
@@ -86,10 +86,10 @@
 
 				<div class="carousel-item carousel3">
 					<svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"/></svg>
-					<img class="d-block w-100" src="images/Five" alt="First slide">
+					<img class="d-block w-100" src="images/wesley2" alt="First slide">
 					<div class="container">
-						<div class="carousel-caption text-right">
-							<h1>Five Finger Death Punch</h1>
+						<div class="carousel-caption text-left">
+							<h1>Wesley safadão</h1>
 							<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
 							<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
 						</div>
@@ -114,20 +114,21 @@
 	  			<!-- Three columns of text below the carousel -->
 	  			<div class="row">
 	  				<div class="col-lg-4">
-	  					<svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-	  					<h2>Heading</h2>
+	  					<img class="d-block w-100" src="images/wendell" style="border-radius: 200px;">
+
+	  					<h2>Wendell Bezerra</h2>
 	  					<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
 	  					<p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
 	  				</div><!-- /.col-lg-4 -->
 	  				<div class="col-lg-4">
-	  					<svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-	  					<h2>Heading</h2>
+	  					<img class="d-block w-100" src="images/marcio" style="border-radius: 200px;">
+	  					<h2>Marcio Seixas</h2>
 	  					<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
 	  					<p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
 	  				</div><!-- /.col-lg-4 -->
 	  				<div class="col-lg-4">
-	  					<svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-	  					<h2>Heading</h2>
+	  					<img class="d-block w-100" src="images/marco" style="border-radius: 200px;">
+	  					<h2>Marco Ribeiro</h2>
 	  					<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
 	  					<p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
 	  				</div><!-- /.col-lg-4 -->
