3435dac35f1baf8efcc401424a2cd63d9e34d734
mateusalencar <matthew.allencar@gmail.com>
2019-06-06 15:58:01 -0300

    Alterações no radio.php

--- radio.php
+++ radio.php
@@ -27,136 +27,143 @@
 		<?php
 		include('include/header.php');
 		?>
+		<section class="videos">
+			<div class="container">
+				<div class="container-hender">
+					<div class="row">
+						<div class="col-sm-9">
+							<div class="line-titulo"><h1 alt="radio ao vivo agora">Ao vivo agora</h1></div><!--line-titulo-->
 
-		<div class="container">
-			<div class="container-hender">
-				<div class="row">
-					<div class="col-sm-9">
-						<div class="line-titulo"><h1 alt="radio ao vivo agora">Ao vivo agora</h1></div><!--line-titulo-->
+							<div class="content-media content-media-video">
+								<iframe  id="aovivo" src="https://www.youtube.com/embed/tgbNymZ7vqY?playlist=tgbNymZ7vqY&loop=1">
+								</iframe>
+							</div><!--content-media-->
 
-						<div class="content-media content-media-video">
-							<iframe  id="aovivo" src="https://www.youtube.com/embed/tgbNymZ7vqY?playlist=tgbNymZ7vqY&loop=1">
-							</iframe>
-						</div><!--content-media-->
+							<h2 class="title style-scope" alt="Chasing Deams">Chasing Dreams</></h2>
 
-						<h2 class="title style-scope" alt="Chasing Deams">Chasing Dreams</></h2>
+							<hr class="featurette-divider"><!--featurette-divider-->
 
-						<hr class="featurette-divider"><!--featurette-divider-->
+							
+						</div><!--col-sm-9-->
 
-						<div class="container">
-							<h2 class="text-left">Comentários</h2>
+						<div class="col-sm-3">
+							<div class="line-titulo"><h2>Videos</h2></div><!--line-titulo-->
+							<div class="video-wraper">
+								<div class="embed-responsive embed-responsive-16by9">
+									<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
+								</div><!--embed-responsive embed-responsive-16by9-->
+								<h2 class="h5">Empurrei minha avó da escada rolante olha no que deu</h2>
+								<p>2 days ago</p>
+							</div><!--video-wraper-->
 
-							<div class="card">
-								<div class="card-body">
-									<div class="row">
-										<div class="col-md-2">
-											<img src="https://image.ibb.co/jw55Ex/def_face.jpg" alt="Comentário de Alfredo Prazeirozo Texugueiro" class="img img-rounded img-fluid"/>
-											<p class="text-secondary text-center">15 Minutes Ago</p>
-										</div><!--col-md-2-->
-										<div class="col-md-10">
-											<p>
-												<a class="float-left" href=""><strong>Alfredo Prazeirozo Texugueiro</strong></a>
-												<span class="float-right"><i class="text-warning fa fa-star"></i></span>
-												<span class="float-right"><i class="text-warning fa fa-star"></i></span>
-												<span class="float-right"><i class="text-warning fa fa-star"></i></span>
-												<span class="float-right"><i class="text-warning fa fa-star"></i></span>
-											</p>
-											<div class="clearfix"></div><!--clearfix-->
-											<p>Lorem Ipsum is simply dummy text of the pr make  but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-											<p>
-												<a class="float-right btn btn-outline-primary ml-2"> <i class="fa fa-reply"></i> Reply</a>
-												<a class="float-right btn text-white btn-danger"> <i class="fa fa-heart"></i> Like</a>
-											</p>
-										</div><!--col-md-10-->
-									</div><!--row-->
-								</div><!--card-body-->
-							</div><!--card-->
+							<div class="video-wraper">
 
-							<div class="card">
-								<div class="card-body">
-									<div class="row">
-										<div class="col-md-2">
-											<img src="https://image.ibb.co/jw55Ex/def_face.jpg" alt="Comentário de Aberta Demais De Oliveira" class="img img-rounded img-fluid"/>
-											<p class="text-secondary text-center">15 Minutes Ago</p>
-										</div><!--col-md-2-->
-										<div class="col-md-10">
-											<p>
-												<a class="float-left" href=""><strong>Aberta Demais De Oliveira</strong></a>
-												<span class="float-right" href=""><i class="text-warning fa fa-star"></i></span>
-												<span class="float-right"><i class="text-warning fa fa-star"></i></span>
-												<span class="float-right"><i class="text-warning fa fa-star"></i></span>
-												<span class="float-right"><i class="text-warning fa fa-star"></i></span>
+								<div class="embed-responsive embed-responsive-16by9">
+									<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
+								</div><!--embed-responsive embed-responsive-16by9-->
+								<h2 class="h5">Empurrei minha avó da escada rolante olha no que deu</h2>
+								<p>4 days ago</p>
+							</div><!--video-wraper-->
 
-											</p>
-											<div class="clearfix"></div><!--clearfix-->
-											<p>Lorem Ipsum is simply dummy text of the pr make  but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-											<p>
-												<a class="float-right btn btn-outline-primary ml-2"> <i class="fa fa-reply"></i> Reply</a>
-												<a class="float-right btn text-white btn-danger"> <i class="fa fa-heart"></i> Like</a>
-											</p>
-										</div><!--col-md-10-->
-									</div><!--row-->
-								</div><!--card-body-->
-							</div><!--card-->
+							<div class="video-wraper">
+								<div class="embed-responsive embed-responsive-16by9">
+									<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
+								</div><!--embed-responsive embed-responsive-16by9-->
+								<h2 class="h5">Empurrei minha avó da escada rolante olha no que deu</h2>
+								<p>5 days ago</p>
+							</div><!--video-wraper-->
+						</div><!--col-sm-3-->
+					</div><!--row-->
+				</div><!--container-render-->
+			</div><!--container-->
+		</section><!--videos-->
 
-							<div class="card">
-								<div class="card-body">
-									<div class="row">
-										<div class="col-md-2">
-											<img src="https://image.ibb.co/jw55Ex/def_face.jpg" alt="Comentário de Aberta Demais De Oliveira" class="img img-rounded img-fluid"/>
-											<p class="text-secondary text-center">15 Minutes Ago</p>
-										</div><!--col-md-2-->
-										<div class="col-md-10">
-											<p>
-												<a class="float-left" href=""><strong>Aberta Demais De Oliveira</strong></a>
-												<span class="float-right" href=""><i class="text-warning fa fa-star"></i></span>
-												<span class="float-right"><i class="text-warning fa fa-star"></i></span>
-												<span class="float-right"><i class="text-warning fa fa-star"></i></span>
-												<span class="float-right"><i class="text-warning fa fa-star"></i></span>
-											</p>
-											<div class="clearfix"></div><!--clearfix-->
-											<p>Lorem Ipsum is simply dummy text of the pr make  but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-											<p>
-												<a class="float-right btn btn-outline-primary ml-2"> <i class="fa fa-reply"></i> Reply</a>
-												<a class="float-right btn text-white btn-danger"> <i class="fa fa-heart"></i> Like</a>
-											</p>
-										</div><!--col-md-10-->
-									</div><!--row-->
-								</div><!--card-body-->
-							</div><!--card-->
-						</div><!--container-->
-					</div><!--col-sm-9-->
+		<section class="comments">
+			<div class="container">
+				<h2 class="text-left">Comentários</h2>
 
-					<div class="col-sm-3">
-						<div class="line-titulo"><h2>Videos</h2></div><!--line-titulo-->
-						<div class="video-wraper">
-							<div class="embed-responsive embed-responsive-16by9">
-								<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
-							</div><!--embed-responsive embed-responsive-16by9-->
-							<h2 class="h5">Empurrei minha avó da escada rolante olha no que deu</h2>
-							<p>2 days ago</p>
-						</div><!--video-wraper-->
+				<div class="card">
+					<div class="card-body">
+						<div class="row">
+							<div class="col-md-2">
+								<img src="https://image.ibb.co/jw55Ex/def_face.jpg" alt="Comentário de Alfredo Prazeirozo Texugueiro" class="img img-rounded img-fluid"/>
+								<p class="text-secondary text-center">15 Minutes Ago</p>
+							</div><!--col-md-2-->
+							<div class="col-md-10">
+								<p>
+									<a class="float-left" href=""><strong>Alfredo Prazeirozo Texugueiro</strong></a>
+									<span class="float-right"><i class="text-warning fa fa-star"></i></span>
+									<span class="float-right"><i class="text-warning fa fa-star"></i></span>
+									<span class="float-right"><i class="text-warning fa fa-star"></i></span>
+									<span class="float-right"><i class="text-warning fa fa-star"></i></span>
+								</p>
+								<div class="clearfix"></div><!--clearfix-->
+								<p>Lorem Ipsum is simply dummy text of the pr make  but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+								<p>
+									<a class="float-right btn btn-outline-primary ml-2"> <i class="fa fa-reply"></i> Reply</a>
+									<a class="float-right btn text-white btn-danger"> <i class="fa fa-heart"></i> Like</a>
+								</p>
+							</div><!--col-md-10-->
+						</div><!--row-->
+					</div><!--card-body-->
+				</div><!--card-->
 
-						<div class="video-wraper">
+				<div class="card">
+					<div class="card-body">
+						<div class="row">
+							<div class="col-md-2">
+								<img src="https://image.ibb.co/jw55Ex/def_face.jpg" alt="Comentário de Aberta Demais De Oliveira" class="img img-rounded img-fluid"/>
+								<p class="text-secondary text-center">15 Minutes Ago</p>
+							</div><!--col-md-2-->
+							<div class="col-md-10">
+								<p>
+									<a class="float-left" href=""><strong>Aberta Demais De Oliveira</strong></a>
+									<span class="float-right" href=""><i class="text-warning fa fa-star"></i></span>
+									<span class="float-right"><i class="text-warning fa fa-star"></i></span>
+									<span class="float-right"><i class="text-warning fa fa-star"></i></span>
+									<span class="float-right"><i class="text-warning fa fa-star"></i></span>
 
-							<div class="embed-responsive embed-responsive-16by9">
-								<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
-							</div><!--embed-responsive embed-responsive-16by9-->
-							<h2 class="h5">Empurrei minha avó da escada rolante olha no que deu</h2>
-							<p>4 days ago</p>
-						</div><!--video-wraper-->
+								</p>
+								<div class="clearfix"></div><!--clearfix-->
+								<p>Lorem Ipsum is simply dummy text of the pr make  but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+								<p>
+									<a class="float-right btn btn-outline-primary ml-2"> <i class="fa fa-reply"></i> Reply</a>
+									<a class="float-right btn text-white btn-danger"> <i class="fa fa-heart"></i> Like</a>
+								</p>
+							</div><!--col-md-10-->
+						</div><!--row-->
+					</div><!--card-body-->
+				</div><!--card-->
 
-						<div class="video-wraper">
-							<div class="embed-responsive embed-responsive-16by9">
-								<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
-							</div><!--embed-responsive embed-responsive-16by9-->
-							<h2 class="h5">Empurrei minha avó da escada rolante olha no que deu</h2>
-							<p>5 days ago</p>
-						</div><!--video-wraper-->
-					</div><!--col-sm-3-->
-				</div><!--row-->
-			</div><!--container-render-->
-		</div><!--container-->
+				<div class="card">
+					<div class="card-body">
+						<div class="row">
+							<div class="col-md-2">
+								<img src="https://image.ibb.co/jw55Ex/def_face.jpg" alt="Comentário de Aberta Demais De Oliveira" class="img img-rounded img-fluid"/>
+								<p class="text-secondary text-center">15 Minutes Ago</p>
+							</div><!--col-md-2-->
+							<div class="col-md-10">
+								<p>
+									<a class="float-left" href=""><strong>Aberta Demais De Oliveira</strong></a>
+									<span class="float-right" href=""><i class="text-warning fa fa-star"></i></span>
+									<span class="float-right"><i class="text-warning fa fa-star"></i></span>
+									<span class="float-right"><i class="text-warning fa fa-star"></i></span>
+									<span class="float-right"><i class="text-warning fa fa-star"></i></span>
+								</p>
+								<div class="clearfix"></div><!--clearfix-->
+								<p>Lorem Ipsum is simply dummy text of the pr make  but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+								<p>
+									<a class="float-right btn btn-outline-primary ml-2"> <i class="fa fa-reply"></i> Reply</a>
+									<a class="float-right btn text-white btn-danger"> <i class="fa fa-heart"></i> Like</a>
+								</p>
+							</div><!--col-md-10-->
+						</div><!--row-->
+					</div><!--card-body-->
+				</div><!--card-->
+			</div><!--container-->
+		</section><!--comments-->
+		
+		
 
 	</main><!--main-->
 
